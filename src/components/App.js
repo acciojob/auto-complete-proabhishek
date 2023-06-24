252dae1ebd053021a722d691e15c8e0400cfda03
Abhishek <28715657+proabhishek@users.noreply.github.com>
2023-06-24 07:13:11 +0000

    t1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
+let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+
 const App = () => {
+   let [search, setSearch] = useState("")
+   let [filtered, setFiltered] = useState(fruits)
+
+    useEffect(()=>{
+      setFiltered(fruits.filter(value=> value.includes(search)))
+    },[search])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <input type="text" placeholder="search..."
+          onChange={e => setSearch(e.target.value)}
+       />
+      <ul>
+        {filtered.map(value =>(
+             <li>{value}</li>
+        ))}
+      </ul>
     </div>
   )
 }
