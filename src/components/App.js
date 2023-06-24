482be025676a59c9dab83ed71b4026a192a1634d
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-24 15:49:13 +0530

    submitting code-Sat Jun 24 2023 15:49:06 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,42 @@
 import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
+
+
 let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 
 const App = () => {
-   let [search, setSearch] = useState("")
-   let [filtered, setFiltered] = useState(fruits)
 
-    useEffect(()=>{
-      setFiltered(fruits.filter(value=> value.includes(search)))
-    },[search])
+  let [search, setSearch] = useState("")
 
-  return (
+  let [filterFruits, setFilterFruits] = useState(fruits)
+
+
+  useEffect(() => {
+
+    setFilterFruits( fruits.filter(value =>  value.toLowerCase().includes(search.toLowerCase()) ))
+        
+  },[search])
+
+
+
+  return(
     <div>
-       <input type="text" placeholder="search..."
-          onChange={e => setSearch(e.target.value)}
-       />
-      <ul>
-        {filtered.map(value =>(
-             <li>{value}</li>
-        ))}
-      </ul>
+             <input type="text" placeholder="Search"
+               onChange={e => setSearch(e.target.value)}
+             />
+             <ul>
+                  { 
+                     filterFruits.map(value =>(
+                        <li>{value}</li>
+                     ))
+                  }
+     
+             </ul>
+             
     </div>
   )
 }
 
+ 
 export default App
